1f0d4be0a5e5d5767842a2f8fb67a63cea72ce1e
Andy <1585638808@qq.com>
2017-08-11 09:43:18 +0800

    init:flower_playing_cards:

---
+++ README.md
@@ -0,0 +1,7 @@
+global install
+
+```
+npm install -g webpack
+```
+
+a webpack3 simple webapp template
---
+++ webpack.conf.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin  = require("extract-text-webpack-plugin");
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
+const utils = require("./utils");
+const config = require("./config");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: config.build.assetsRoot,
+        filename: utils.assetsPath('js/[name].[chunkhash].js'),
+        chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+             template: 'src/index.html'
+        }),
+        new ExtractTextPlugin(config.build.assetsSubDirectory + "/css/[name]-[chunkhash:8].css"),
+        // new StyleExtHtmlWebpackPlugin({
+        //     position: 'head-bottom'
+        // })
+    ]
+};
